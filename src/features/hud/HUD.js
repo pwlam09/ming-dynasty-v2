787865860d815259db8acc1628ca13fd1a316505
Idy <laughgirlidy@gmail.com>
2022-04-14 10:35:56 +0800

    Fix 100vh UI

--- src/features/hud/HUD.js
+++ src/features/hud/HUD.js
@@ -1,17 +1,37 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import useWindowDimensions from "../../utils/resize"
+import { PingPong } from "../pingpong/PingPong"
 import { MatchThreeGame } from "../squares/MatchThreeGame"
 
 export const HUD = () => {
+    const gameAreaToWindowHeightRatio = 0.6
+    const mainAreaRef = useRef()
     const ref = useRef()
+    const { height: screenHeight, width: screenWidth } = useWindowDimensions()
+    const [gameAreaWidth, setGameAreaWidth] = useState(0)
+
+    useEffect(() => {
+        const minGameAreaWidth = screenHeight < screenWidth ? screenHeight * gameAreaToWindowHeightRatio : screenWidth
+        setGameAreaWidth(minGameAreaWidth)
+    })
 
     return (
-        <div className="main-area">
+        <div ref={mainAreaRef} className="main-area">
             <div className="view-area">
                 Character/Enemy/Story View
             </div>
-            <div className="game-area" ref={ref}>
+            <div 
+                className="game-area" 
+                ref={ref}
+                style={{
+                    width: gameAreaWidth
+                }}>
                 {/* Game Area */}
-                <MatchThreeGame parentRef={ref} />
+                {/* <MatchThreeGame parentRef={ref} /> */}
+                <PingPong 
+                    gameAreaWidth={gameAreaWidth} 
+                    mainAreaRef={mainAreaRef} 
+                />
             </div>
         </div>
     )
